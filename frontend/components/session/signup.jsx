2392b6cc1958e74025f381e89f4cedde60637219
Syldys <syldysnya@gmail.com>
2021-07-04 02:08:28 -0400

    merge again

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCities } from '../../actions/city_actions';
@@ -225,168 +224,13 @@ const Signup = () => {
                         <div className='btn-sign-in-login' value='Sign Up'
                             onClick={handleModal}> 
                             Sign In
-=======
-import React from 'react';
-
-class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            email: '',
-            password: '',
-            fname: '',
-            lname: '',
-            city_id: '',
-            confirmPassword: ''
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.demoSubmit = this.demoSubmit.bind(this);
-        this.handleInputCity = this.handleInputCity.bind(this)
-    }
-
-    handleErrors(field) {
-        return this.props.errors.filter(error => error.includes(field))
-    }
-
-    componentDidMount() {
-        this.props.fetchCities();
-    }
-
-    componentWillUnmount() {
-        this.props.removeErrors();
-    }
-
-    demoSubmit(e) {
-        e.preventDefault();
-        this.props.loginDemo().then(this.props.hideModal);
-    }
-
-    handleInput(field) {
-        return e => this.setState({ [field]: e.target.value })
-    }
-
-    handleInputCity(e) {
-        
-        const idx = parseInt(e.target.value);
-        this.setState({ city_id: this.props.city_indices[idx] })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.signup(this.state).then(this.props.hideModal);
-    }
-
-    render() {
-        return (
-            <div id='signup-modal-background'>
-                <div className='row'>
-                    <div className='first-message-column-modal'>
-                        <h1>Welcome to OpenTable!</h1>
-                    </div>
-                    <div className='row'>
-                        <form className='user-form'>
-                            <label htmlFor="fname-login">
-                                <input
-                                    className='modal-input'
-                                    type="text"
-                                    id="fname-login"
-                                    placeholder='First Name *'
-                                    onChange={this.handleInput('fname')}
-                                />
-                                <li className='auth-error' key='fname-error'>
-                                    {this.handleErrors('Fname')}
-                                </li>
-                            </label>
-                            <label htmlFor="lname-login">
-                                <input
-                                    className='modal-input'
-                                    type="text"
-                                    id="lname-login"
-                                    placeholder='Last Name *'
-                                    onChange={this.handleInput('lname')}
-                                />
-                                <li className='auth-error' key='lname-error'>
-                                    {this.handleErrors('Lname')}
-                                </li>
-                            </label>
-                            <label htmlFor="email-login">
-                                <input
-                                    className='modal-input'
-                                    type="text"
-                                    id="email-login"
-                                    placeholder='Enter Email *'
-                                    onChange={this.handleInput('email')}
-                                />
-                                <li className='auth-error' key='email-error'>
-                                    {this.handleErrors('Email')}
-                                </li>
-                            </label>
-                            <label htmlFor="password-login">
-                                <input
-                                    className='modal-input'
-                                    type="password"
-                                    id="password-login"
-                                    placeholder='Enter password *'
-                                    onChange={this.handleInput('password')}
-                                />
-                                <li className='auth-error' key='password-error'>
-                                    {this.handleErrors('Password')}
-                                </li>
-                            </label>
-                            <label htmlFor="password-login2">
-                                <input
-                                    className='modal-input'
-                                    type="password"
-                                    id="password-login2"
-                                    placeholder='Re-enter password *'
-                                    onChange={this.handleInput('confirmPassword')}
-                                />
-                                <li className='auth-error' key='password-error'>
-                                    {this.handleErrors('Password')}
-                                </li>
-                            </label>
-                            <label htmlFor="city-login">
-                                <select onChange={this.handleInputCity}
-                                    className='modal-input'>
-                                    <option disabled defaultValue='Primary city'>Primary city*</option>
-                                    {this.props.cities.map((city, i) => (
-                                        <option key={`city-${i}`} value={i}>
-                                            {city}
-                                        </option>
-                                    ))}
-                                </select>
-                            </label>
-                            <li className='auth-error' key='city-error'>
-                                {this.handleErrors('City')}
-                            </li>
-                            <button
-                                className='auth-button'
-                                onClick={this.handleSubmit}>
-                                    Create Account
-                            </button>
-                        </form>
-                        <div className='demouser-form'>
-                            <div>Don't want to complete the form?</div>
-                            <button
-                                className='auth-button demouser-button'
-                                onClick={this.demoSubmit}>
-                                Continue with <span>DemoUser</span>
-                        </button>
->>>>>>> 725610b6f5481f09532c1b7d782bbd7c68170a9b
                         </div>
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
         </div>
     )
 
-=======
-        )
-    }
->>>>>>> 725610b6f5481f09532c1b7d782bbd7c68170a9b
 };
 
 export default Signup;
